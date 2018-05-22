8cb728b91817988c4559811584a5567a27b1cd33
Songlin Wu <527502779@qq.com>
2018-05-22 16:27:22 +1000

    Initial Commit

---
+++ Accounting Bill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Accounting Bill
+//
+//  Created by Songlin Wu on 22/5/18.
+//  Copyright © 2018 Songlin Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
